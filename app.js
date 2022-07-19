d4a76f2b76dc6292c12acf8a472fa08468cc55fd
unknown <evgeta@yandex.ru>
2022-07-19 08:35:11 +0300

    feat: eslint error in app.js corrected

--- app.js
+++ app.js
@@ -2,24 +2,21 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
-const usersRouter = require('./routes/users');
-const cardsRouter = require('./routes/cards');
-
 const app = express();
 
-// Слушаем 3000 порт 
+// Слушаем 3000 порт
 const { PORT = 3000 } = process.env;
 
 mongoose.connect('mongodb://localhost:27017/mestodb', {
-  useNewUrlParser: true
+  useNewUrlParser: true,
 });
 
 app.use(bodyParser.json());
 
-//временное решение для авторизации
+// временное решение для авторизации
 app.use((req, res, next) => {
   req.user = {
-    _id: '62d534ecb17d7ddf882d92d8' // вставьте сюда _id созданного в предыдущем пункте пользователя
+    _id: '62d534ecb17d7ddf882d92d8', // вставьте сюда _id созданного в предыдущем пункте пользователя
   };
   next();
 });
@@ -27,9 +24,6 @@ app.use((req, res, next) => {
 app.use('/users', require('./routes/users'));
 app.use('/cards', require('./routes/cards'));
 
-
 app.listen(PORT, () => {
-    console.log(`App listening on port ${PORT}`)
-}) 
-
-
+  // console.log(`App listening on port ${PORT}`);
+});
