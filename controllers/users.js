a79301eb0e24397e244240de177f57c04e9bb275
unknown <evgeta@yandex.ru>
2022-07-30 14:26:58 +0300

    feat: add lofin controller

--- controllers/users.js
+++ controllers/users.js
@@ -1,6 +1,11 @@
+require('dotenv').config();
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const User = require('../models/user');
+
+const { NODE_ENV, JWT_SECRET } = process.env;
+const secret = NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret';
 
 const {
   INCORRECT_DATA_ERROR_CODE,
@@ -139,5 +144,37 @@ module.exports.updateAvatar = (req, res) => {
       return res.status(DEFAULT_ERROR_CODE).send({
         message: 'Ошибка сервера',
       });
+    });
+};
+
+module.exports.login = (req, res) => {
+  const {
+    email,
+    password,
+  } = req.body;
+  User.findOne({ email })
+    .then((user) => {
+      if (!user) {
+        return Promise.reject(new Error('Неправильные почта или пароль'));
+      }
+      return bcrypt.compare(password, user.password)
+        .then((matched) => {
+          if (!matched) {
+            // хеши не совпали — отклоняем промис
+            return Promise.reject(new Error('Неправильные почта или пароль'));
+          }
+          // аутентификация успешна
+          const token = jwt.sign({ _id: 'd285e3dceed844f902650f40' }, secret, { expiresIn: '7d' });
+          res.cookie('jwt', token, {
+            maxAge: 3600000,
+            httpOnly: true,
+          });
+          return res.send({ message: 'Вы успешно авторизовались.' });
+        })
+        .catch((err) => {
+          res
+            .status(401)
+            .send({ message: err.message });
+        });
     });
 };
