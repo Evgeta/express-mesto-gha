7344f46cf73356432428676d1ae3d1715e3f893c
unknown <evgeta@yandex.ru>
2022-08-01 16:57:01 +0300

    feat: move routes to separate file

--- app.js
+++ app.js
@@ -3,15 +3,16 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
-const { celebrate, Joi, errors } = require('celebrate');
+// const { celebrate, Joi, errors } = require('celebrate');
 const helmet = require('helmet');
-const auth = require('./middlewares/auth');
-const { login, createUser } = require('./controllers/users');
-const NotFoundError = require('./errors/NotFoundError');
+// const auth = require('./middlewares/auth');
+// const { login, createUser } = require('./controllers/users');
+// const NotFoundError = require('./errors/NotFoundError');
 const errorHandler = require('./middlewares/errorHandler');
-const { linkRegEx } = require('./utils/regulars');
+// const { linkRegEx } = require('./utils/regulars');
 
 const app = express();
+const router = require('./routes/index');
 
 // Слушаем 3000 порт
 const { PORT = 3000 } = process.env;
@@ -24,30 +25,33 @@ app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(helmet());
 
-app.post('/signin', celebrate({
-  body: Joi.object().keys({
-    email: Joi.string().required().email(),
-    password: Joi.string().required(),
-  }),
-}), login);
+// app.post('/signin', celebrate({
+//   body: Joi.object().keys({
+//     email: Joi.string().required().email(),
+//     password: Joi.string().required(),
+//   }),
+// }), login);
 
-app.post('/signup', celebrate({
-  body: Joi.object().keys({
-    email: Joi.string().required().email(),
-    password: Joi.string().required(),
-    name: Joi.string().min(2).max(30),
-    about: Joi.string().min(2).max(30),
-    avatar: Joi.string().pattern(linkRegEx),
-  }),
-}), createUser);
+// app.post('/signup', celebrate({
+//   body: Joi.object().keys({
+//     email: Joi.string().required().email(),
+//     password: Joi.string().required(),
+//     name: Joi.string().min(2).max(30),
+//     about: Joi.string().min(2).max(30),
+//     avatar: Joi.string().pattern(linkRegEx),
+//   }),
+// }), createUser);
 
-app.use(auth);
-app.use('/users', require('./routes/users'));
-app.use('/cards', require('./routes/cards'));
+// app.use(auth);
+// app.use('/users', require('./routes/users'));
+// app.use('/cards', require('./routes/cards'));
 
-app.use(() => { throw new NotFoundError('Страница не найдена'); });
+// app.use(() => { throw new NotFoundError('Страница не найдена'); });
 
-app.use(errors());
+// app.use(errors());
+
+app.use(router);
+
 app.use(errorHandler);
 
 app.listen(PORT, () => {
