fe02c63a967f21a8db480cf5c9f00725db3139a5
unknown <evgeta@yandex.ru>
2022-08-07 08:56:43 +0300

    feat: prepare to combine frontend and backend

--- app.js
+++ app.js
@@ -16,7 +16,6 @@ const { PORT = 3000 } = process.env;
 mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
 });
-s
 app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(helmet());
