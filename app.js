c8f93b8bad587ce43a40287aea89ad2eddd6c6ea
unknown <evgeta@yandex.ru>
2022-08-07 08:53:22 +0300

    feat: add crash-test

--- app.js
+++ app.js
@@ -16,12 +16,18 @@ const { PORT = 3000 } = process.env;
 mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
 });
-
+s
 app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(helmet());
 
 app.use(requestLogger); // подключаем логгер запросов
+
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
 
 app.use(router);
 
